e2d87a0787000e4acaf2427a95bf9a3617af734e
abusarke <ibrahimabubakar741@gmail.com>
2026-04-08 16:19:02 +0100

    Remove comments and improve script output

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,20 +2,6 @@
 # This script calculates simple interest given principal,
 # annual rate of interest and time period in years.
 
-# Do not use this in production. Sample purpose only.
-
-# Abubakar Ibrahim
-# Additional Authors:
-# abusarke
-
-# Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
-
-# Output:
-# simple interest = p*t*r
-
 echo "Enter the principal:"
 read p
 echo "Enter rate of interest per year:"
